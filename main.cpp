7480c80a3bbb9faef05b0ffcb824c1ddc40065af
Agasya27 <127315310+Agasya27@users.noreply.github.com>
2023-03-08 19:06:54 +0530

    student management system cpp program

--- main.cpp
+++ main.cpp
@@ -1,304 +1,135 @@
 #include <iostream>
 
 using namespace std;
-//global varibles
-int si,id,sal,ch1,id1,i,ch2,ch3;
-   string name,dob,name1,dip1;
 
-
-void ems()
+int menu()  //function
 {
-    cout<<"::Employee Management System"<<endl;
-    cout<<"1.Admin"<<endl;
-    cout<<"2.Manager"<<endl;
-    cout<<"3.Employee"<<endl;
-    cout<<"0.exit"<<endl;
-}
-struct ems
-{
-    int si,id,sal,ch1,id1,i,ch2,ch3;
-   string name,dob,name1,dip;
+    cout<<"::MENU::\n";
+    cout<<"1.Add student\n";
+    cout<<"2.Show All student\n";
+    cout<<"3.Result\n";
+    cout<<"4.update student data\n";
+    cout<<"0.Exit\n";
 
-}e[100];
-class Admin  //Admin declaration
-{
-
-    public:
-    void Adminmenu();
-    void addnewrecord();
-    void particularid();
-    void displayallrecord();
-    void updaterecord();
-
-};
-void Admin::Adminmenu()
-{
-
-    cout<<"::Admin::"<<endl;
-    cout<<"1.add new record"<<endl;
-    cout<<"2.search employee details using particular id"<<endl;
-    cout<<"3.display All employee record"<<endl;
-    cout<<"4.update record"<<endl;
-    cout<<"0.exit"<<endl;
-
-    cout<<"enter your choice"<<endl;
-    cin>>ch1;
-
-    switch(ch1)
+ /* int ch;
+    cout<<"enter your choice:\n";
+    cin>>ch;
+    switch(ch)
     {
     case 1:
-
-        addnewrecord();
-        break;
+    cout<<"selection add student\n";
+    break;
     case 2:
-        particularid();
-        break;
+    cout<<"selection show all student data\n";
+    break;
     case 3:
-        displayallrecord();
-        break;
+    cout<<"selection result \n";
+    break;
     case 4:
-        updaterecord();
-        break;
+    cout<<"selection update student data\n";
+    break;
     default:
-        {
-            cout<<"process terminated successfully" <<endl;
-           // goto c;
-            break;
+    cout<<"selection exit\n";
+    }
+*/
 
-        }
+/*
+    int g;
 
+       cout<<"enter the size of student data required";
+       cin>>g;
+*/
+return 0;
     }
 
 
-}
-
-void Admin::addnewrecord()
-
-{
-
-
-
-    cout<<"enter size"<<endl;
-    cin>>si;
-    for(i=0;i<si;i++)
+    struct student  //structure
     {
-    cout<<"__________________________________________________________________________________"<<endl;
-    cout<<"enter name of employee"<<endl;
-    cin>>e[i].name;
-    cout<<"enter id of employee"<<endl;
-    cin>>e[i].id;
-    cout<<"enter date of birth"<<endl;
-    cin>>e[i].dob;
-    cout<<"enter department"<<endl;
-    cin>>e[i].dip;
-    cout<<"enter salary"<<endl;
-    cin>>e[i].sal;
-    }
-    cout<<"__________________________________________________________________________________"<<endl;
-
-    Adminmenu();
-}
-
-void Admin::particularid()
-
-{
-
-
-    cout<<"enter id"<<endl;
-    cin>>id1;
-
-
-for(i=0;i<si;i++)
-    {
-       if(id1==e[i].id)
-       {
-         cout<<"name= \t"<<e[i].name<<"id= \t"<<e[i].id<<"DOB= \t"<<e[i].dob<<"salary= \t"<<e[i].sal<<"Department= \t"<<e[i].dip<<endl;
-
-       }
-    }
-
-    cout<<"__________________________________________________________________________________"<<endl;
-
-
-    Adminmenu();
-}
-
-void Admin::displayallrecord()
-{
-    for(i=0;i<si;i++)
-    {
-        cout<<"name= \t"<<e[i].name<<"id= \t"<<e[i].id<<"DOB= \t"<<e[i].dob<<"salary= \t"<<e[i].sal<<"Department= \t"<<e[i].dip<<endl;
-    }
-
-    cout<<"__________________________________________________________________________________"<<endl;
-
-
-    Adminmenu();
-}
-
-void Admin::updaterecord()
-{
-
-
-  cout<<"enter id"<<endl;
-  cin>>id1;
-  if(id1==e[i].id)
-    {
-        cout<<"name=    "<<e[i].name<<"id=   "<<e[i].id<<"DOB=    "<<e[i].dob<<"salary=   "<<e[i].sal<<"department=\t"<<e[i].dip<<endl;
-    }
-  cout<<"enter name to be updated"<<endl;
-  cin>>name1;
-  cout<<"records updated succesfully"<<endl;
-  for(i=0;i<si;i++)
-  {
-  cout<<"name=     "<<name1<<"id=       "<<e[i].id<<"DOB=    "<<e[i].dob<<"salary=   "<<e[i].sal<<"department=\t"<<e[i].dip<<endl;
-
-  }
-cout<<"__________________________________________________________________________________"<<endl;
-ems();
-}
-
-// Manager declaration
-class Manager:public Admin
-{
-public:
-    void Managermenu();
-    void detailsbydip();
-};
-void Manager::Managermenu()
-{
-     cout<<"::Manager::"<<endl;
-   // cout<<"1.Add new record"<<endl;
-    cout<<"1.Employee Details by id"<<endl;
-    cout<<"2.Employee Details Department"<<endl;
-    cout<<"3.Display all records"<<endl;
-    cout<<"0.exit"<<endl;
-
-    cout<<"enter your choice"<<endl;
-    cin>>ch2;
-    switch(ch2)
-    {
-   // case 1:
-        //addnewrecord();
-       // break;
-    case 1:
-        particularid();
-        break;
-    case 2:
-        detailsbydip();
-        break;
-    case 3:
-        displayallrecord();
-        break;
-    default:
-        {
-            cout<<"process terminated successfully"<<endl;
-            //goto k;
-            break;
-        }
-        Managermenu();
-    }
-
-}
-void Manager::detailsbydip()
-{
-    cout<<"enter department"<<endl;
-    cin>>dip1;
-    for(i=0;i<si;i++)
-    {
-    if(dip1==e[i].dip)
-    {
-      cout<<"name=     "<<name1<<"id=       "<<e[i].id<<"DOB=    "<<e[i].dob<<"salary=   "<<e[i].sal<<"department=\t"<<e[i].dip<<endl;
-    }
-
-    }
-   ems();
-
- // Managermenu();
-}
-//employee declaration
-class Employee:public Manager
-{
-public:
-   void Employeemenu();
-
-};
-void Employee::Employeemenu()
-{
-    cout<<"::Employee::"<<endl;
-    cout<<"1.add new record"<<endl;
-    cout<<"2.search employee details using particular id"<<endl;
-    cout<<"3.display All employee record"<<endl;
-    cout<<"4.update record"<<endl;
-    cout<<"0.exit"<<endl;
-
-    cout<<"enter your choice"<<endl;
-    cin>>ch3;
-    switch(ch3)
-    {
-    case 1:
-        addnewrecord();
-        break;
-    case 2:
-        particularid();
-        break;
-    case 3:
-        displayallrecord();
-        break;
-    case 4:
-        updaterecord();
-        break;
-        default:
-        {
-            cout<<"process terminated successfully"<<endl;
-           // goto m;
-            break;
-        }
-
-    }
-    ems();
-}
+        char name[50];
+        int roll;
+        int ds;
+        int db;
+        int op;
+    };
 
 int main()
 {
-   Employee a;
 
-   ems();
+    struct student a[100];
+    int i,n;
+     int ch;
 
-
-   int ch;
-   l:cout<<"enter your choice"<<endl;
-   cin>>ch;
-   switch(ch)
-   {
-   case 1:
-    cout<<"you have selected Admin"<<endl;
-    a.Adminmenu();
-    cout<<endl;
-    ems();
-    goto l;
+    menu();
+    cout<<"enter your choice:\n";
+    cin>>ch;
+    switch(ch)
+    {
+    case 1:
+    cout<<"selection add student\n";
     break;
-   case 2:
-    cout<<"you have selected Manager"<<endl;
-    a.Managermenu();
-    cout<<endl;
-    ems();
-    goto l;
+    case 2:
+    cout<<"selection show all student data\n";
     break;
-   case 3:
-    cout<<"you have selected Employee"<<endl;
-    a.Employeemenu();
-    cout<<endl;
-    ems();
-    goto l;
+    case 3:
+    cout<<"selection result \n";
+    break;
+    case 4:
+    cout<<"selection update student data\n";
     break;
     default:
-    {
-        cout<<"::THANK YOU ALL OPERATION'S TERMINATED SUCCESSFULLY::"<<endl;
-        break;
+    cout<<"selection exit\n";
     }
-
-   }
+    cout<<"\nenter the data required ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cout<<"\nenter the name:";
+        cin>>a[i].name;
+        cout<<"\nenter the roll no:";
+        cin>>a[i].roll;
+        cout<<"\nenter the marks of data structure:";
+        cin>>a[i].ds;
+        cout<<"\nenter the marks of data base management:";
+        cin>>a[i].db;
+        cout<<"\nenter the marks of object programming:";
+        cin>>a[i].op;
+        cout<<"------------------------------------------------------------------------------------\n";
+    }
+    for(i=0;i<n;i++)
+    {
+        cout<<"\n name:"<<a[i].name;
+        cout<<"\n rollno:"<<a[i].roll;
+        cout<<"\n marks of DSU:"<<a[i].ds;
+        cout<<"\n marks of DBMS:"<<a[i].db;
+        cout<<"\n marks of OOP:"<<a[i].op;
+        cout<<"\n------------------------------------------------------------------------------------";
+    }
+   cout<<"\nrecord saved!";
 
     return 0;
 }
+
+
+
+ /*
+    int ch;
+    cout<<"enter your choice:\n";
+    cin>>ch;
+    switch(ch)
+    {
+    case 1:
+    cout<<"selection add student\n";
+    break;
+    case 2:
+    cout<<"selection show all student data\n";
+    break;
+    case 3:
+    cout<<"selection result \n";
+    break;
+    case 4:
+    cout<<"selection update student data\n";
+    break;
+    default:
+    cout<<"selection exit\n";
+    }
+    */
